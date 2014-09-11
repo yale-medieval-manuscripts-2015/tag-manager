1bcd33eaa81a4a8de0bad5f83af7a1c388434ce7
rlechich <roy.lechich@yyale.edu>
2014-09-11 15:39:06 -0400

    heroku 8

--- app/models/solr_mapping.rb
+++ app/models/solr_mapping.rb
@@ -1,3 +1,4 @@
 class SolrMapping < ActiveRecord::Base
   belongs_to :tag
+  attr_accessible :solrfield, :solrvalue, :tag_id
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ActiveRecord::Base
   has_many :solr_mappings, dependent: :destroy
+  attr_accessible :tag, :label, :category
 end
