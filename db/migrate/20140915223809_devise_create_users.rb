fa09ae7118327e41f5536b5db411af2df0df8612
rlechich <roy.lechich@yyale.edu>
2014-09-18 12:12:10 -0400

    fix migrate file

--- db/migrate/20140915223809_devise_create_users.rb
+++ db/migrate/20140915223809_devise_create_users.rb
@@ -44,3 +44,4 @@ class DeviseCreateUsers < ActiveRecord::Migration
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
+  end
