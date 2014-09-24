ec40f59b2ded9187e9dcccbc8b9f6e5a57fb6abc
rlechich <roy.lechich@yyale.edu>
2014-09-24 10:45:25 -0400

    add validation, tag description field and make tag category and solr_mapping value dropdowns

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   has_many :solr_mappings, dependent: :destroy
-  attr_accessible  :category, :tag, :label
+  attr_accessible  :category, :tag, :label, :description
+  validates :category, length: { minimum: 2 }
+  validates :tag, length: { minimum: 2 }, format: { :with => /\A\#/, message: "must start with '#''"}
+  validates :label, length: { minimum: 1 }
+  validates :label, length: { minimum: 2 }
 end
