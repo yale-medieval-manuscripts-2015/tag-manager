e21f83f7ce39e07bc6f1b527f1162b7cd0f10d9a
rlechich <roy.lechich@yyale.edu>
2014-10-03 17:09:05 -0400

    redo

--- db/migrate/20141003191930_solr_mappings_rename_column.rb
+++ db/migrate/20141003191930_solr_mappings_rename_column.rb
@@ -1,4 +1,4 @@
-class ChangeSolrMappingsRenameColumn < ActiveRecord::Migration
+class SolrMappingsRenameColumn < ActiveRecord::Migration
   def change
     def change
       rename_column :solr_mappings, :solrfield, :index_category
